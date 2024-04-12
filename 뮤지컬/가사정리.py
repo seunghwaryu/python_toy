619f8eac3d40021e32fb080e8db58e3a5158c64c
Seunghwa Ryu <rshh0714@gmail.com>
2024-04-12 23:16:28 +0900

    제목 마지막에 입력받도록 수정

--- 뮤지컬/가사정리.py
+++ 뮤지컬/가사정리.py
@@ -1,9 +1,6 @@
 #가사 정리용 (등장인물 이름 및 불필요한 줄바꿈 없앰)
 #사용법: 가사 복붙 후 0 입력
 
-name = input()
-f= open("D:/Me/가사/"+name+".txt","w+") #파일 생성
-
 exceptList = ["다이애나", "댄", "나탈리", "게이브",'헨리','파인 박사','의사들'] # 제거할 등장인물 이름 넣으면 됨
 haha = [] # 추출된 가사들
 while True:
@@ -22,7 +19,10 @@ while True:
             break
     if flag:
         haha.append(line)
-        
+
+name = input()
+f= open("D:/Me/가사/"+name+".txt","w+") #파일 생성
+
 for i in haha:
     f.write(i+"\n")
 f.close()
